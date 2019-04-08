cc3d8b66d9840c1061fe17b0111cf47f835f1fbd
Adam S <adam.sarli@gmail.com>
2019-04-08 17:40:55 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -22,7 +22,16 @@ CREATE TABLE books (
   series_id TEXT
 );
 
+CREATE TABLE characters ( 
+  id INTEGER PRIMARY KEY, 
+  name TEXT,
+  motto TEXT,
+  species TEXT,
+  author_id INTEGER,
+  series_id INTEGER
+);
 
+CREATE TABLE books (
 
-
+);
 
