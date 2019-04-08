7c5a9a7a6bbb71c5596e4c995915c4d46a308d9b
Adam S <adam.sarli@gmail.com>
2019-04-08 17:39:49 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -1,6 +1,28 @@
-CREATE TABLE Series ( 
+CREATE TABLE series ( 
   id INTEGER PRIMARY KEY,
   title TEXT,
   author_id TEXT,
   subgenre_id TEXT
 );
+
+CREATE TABLE subgenres (
+  id INTEGER PRIMARY KEY, 
+  name TEXT
+);
+
+CREATE TABLE authors (
+  id INTEGER PRIMARY KEY, 
+  name TEXT
+);
+
+CREATE TABLE books (
+  id INTEGER PRIMARY KEY, 
+  title TEXT, 
+  year INTEGER, 
+  series_id TEXT
+);
+
+
+
+
+
