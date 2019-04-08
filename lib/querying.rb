393cfedc996e9791f3a2154f7f06151c0974b1b3
Adam S <adam.sarli@gmail.com>
2019-04-08 18:19:39 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -5,7 +5,9 @@ ORDER BY year;"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "Write your SQL query here"
+  "SELECT characters.name, characters.motto
+FROM characters
+ORDER BY LENGTH(characters.motto) DESC LIMIT 1;"
 end
 
 
