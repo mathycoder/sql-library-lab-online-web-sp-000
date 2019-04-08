b5439cd164c17973005819b850e3d3068b2ccd5b
Adam S <adam.sarli@gmail.com>
2019-04-08 18:36:15 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -26,8 +26,9 @@ end
 def select_series_title_with_most_human_characters
   "SELECT series.title FROM series 
 INNER JOIN characters ON series.id = characters.series_id
-GROUP BY characters.species
-ORDER BY COUNT(characters.species) DESC LIMIT 1;"
+WHERE characters.species = 'human'
+GROUP BY series.title
+ORDER BY COUNT(characters.species) DESC LIMIT 1"
 end
 
 def select_character_names_and_number_of_books_they_are_in
